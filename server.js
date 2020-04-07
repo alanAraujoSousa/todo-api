7e2c7163965446caf914a34eb163886a706caf14
alan <alan.araujosb@gmail.com>
2020-04-07 12:15:01 -0300

    init node api project

---
+++ server.js
@@ -0,0 +1,18 @@
+require('rootpath')();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const server = app.listen(port, function () {
+    console.log('The todo-api is listening on port ' + port);
+});
+
+module.exports = app
